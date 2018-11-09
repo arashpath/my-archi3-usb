516b52b0904a79dcb6de87268ad3a1d854e0f2f0
Prashaath R <fssai.cdc@gmail.com>
2018-11-09 17:00:41 +0530

    base installation test completed

--- test/test.sh
+++ test/test.sh
@@ -11,14 +11,61 @@ echo "$sha1sum  $archiso" > iso/"$archiso"_sha1sums.txt
 cd iso; sha1sum -c "$archiso"_sha1sums.txt ; cd ..
 
 # Creating Vatiable File for packer
-cat <<EOF >iso.json
+cat <<EOF >test.json
 {
-    "iso_url": "file://iso/$archiso",
-    "iso_checksum_url": "$ArchMirror/sha1sums.txt",
-    "iso_checksum_type": "sha1",
-    "headless": "true"
+    "variables": {
+        "iso_url": "file://iso/$archiso",
+        "iso_checksum_url": "$ArchMirror/sha1sums.txt",
+        "iso_checksum_type": "sha1",
+        "disk_size": "8192",
+        "memory": "512",
+        "cpus": "1",
+        "write_zeroes": "",
+        "boot_wait": "30s",
+        "headless": "false",
+        "adminuser": "admin",
+        "adminpassword": "admin@123"
+    },
+    "builders": [
+        {
+            "type": "virtualbox-iso",
+            "boot_wait": "{{user `boot_wait`}}",
+            "http_directory": "../scripts",
+            "disk_size": "{{user `disk_size`}}",
+            "guest_os_type": "ArchLinux_64",
+            "iso_checksum_url": "{{user `iso_checksum_url`}}",
+            "iso_checksum_type": "{{user `iso_checksum_type`}}",
+            "iso_url": "{{user `iso_url`}}",
+            "ssh_username": "{{user `adminuser`}}",
+            "ssh_password": "{{user `adminpassword`}}",
+            "ssh_port": 22,
+            "ssh_wait_timeout": "10000s",
+            "shutdown_command": "sudo systemctl poweroff",
+            "guest_additions_mode": "disable",
+            "headless": "{{user `headless`}}",
+            "vboxmanage": [
+                [
+                    "modifyvm",
+                    "{{.Name}}",
+                    "--memory",
+                    "{{user `memory`}}"
+                ],
+                [
+                    "modifyvm",
+                    "{{.Name}}",
+                    "--cpus",
+                    "{{user `cpus`}}"
+                ]
+            ],
+            "boot_command": [
+                "<enter><wait10><wait10><wait10><wait10><wait10><enter><enter>",
+                "curl -O 'http://{{.HTTPIP}}:{{.HTTPPort}}/install_base.sh'<enter><wait>",
+                "bash install_base.sh && systemctl reboot<enter>"
+            ]
+        }
+    ]
 }
 EOF
 
 # Running packer
-packer build -var-file=iso.json archi3usb.json
+packer build test.json
