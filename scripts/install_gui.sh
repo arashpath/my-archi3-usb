e852225d73e70ba2e6e480ceee2856fb923112a2
Prashaath R <arashpath@gmail.com>
2018-11-16 11:35:29 +0530

    Update install_gui.sh

--- scripts/install_gui.sh
+++ scripts/install_gui.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# Gui
+# Gui (~12MB, ~30MB)
 pacman -S i3-gaps xorg-server xorg-apps xorg-xinit ttf-dejavu ttf-inconsolata --noconfirm --needed
+mkdir -p ~/.config/i3/
+cp /etc/i3/config ~/.config/i3
+
+
 echo "
 xsetroot -solid "#333333"
 feh --bg-fill ~/.config/wall.jpg
 exec i3" > ~/.xinitrc
 
 
+# Wallpaper (~4MB, ~13MB)
+pacman -S feh wget --noconfirm --needed
+wget -c "https://tweakers.net/ext/f/ph5BxUGTgCHBPHXMJ2bn4h5C/full.jpg" -P ~/.config/
+echo "exec --no-startup-id feh --bg-fill ~/.config/wall.jpg" >> ~/.config/i3/config
+
 ##
-# Use `feh` to define a background picture for your desktop.
 # Use a compositor manager such as `compton` to enable effects like window fading and transparency.
 # Use `dmenu` or `rofi` to enable customizable menus that can be launched from a keyboard shortcut.
 # Use `dunst` for desktop notifications.
