b55a4375e4de07b63cd3addd194890ccfcada492
Prashaath R <fssai.cdc@gmail.com>
2018-11-16 17:42:26 +0530

    Basic Gui

--- scripts/install_gui.sh
+++ scripts/install_gui.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 # Gui (~12MB, ~30MB)
-pacman -S i3-gaps xorg-server xorg-apps xorg-xinit ttf-dejavu ttf-inconsolata --noconfirm --needed
+sudo pacman -S i3-gaps \
+    xorg-server xorg-apps xorg-xinit \
+    ttf-dejavu ttf-inconsolata --noconfirm --needed
 mkdir -p ~/.config/i3/
 cp /etc/i3/config ~/.config/i3
 
-
-echo "
-xsetroot -solid "#333333"
-feh --bg-fill ~/.config/wall.jpg
-exec i3" > ~/.xinitrc
-
-
 # Wallpaper (~4MB, ~13MB)
+bg_url="https://tweakers.net/ext/f/ph5BxUGTgCHBPHXMJ2bn4h5C/full.jpg"
 pacman -S feh wget --noconfirm --needed
-wget -c "https://tweakers.net/ext/f/ph5BxUGTgCHBPHXMJ2bn4h5C/full.jpg" -P ~/.config/
-echo "exec --no-startup-id feh --bg-fill ~/.config/wall.jpg" >> ~/.config/i3/config
+wget -O ~/.config/wall.jpg -c $bg_url
+echo "# Set Wallpaper
+exec --no-startup-id feh --bg-fill ~/.config/wall.jpg" >> ~/.config/i3/config
+
+# Setup for StartX
+echo '
+xsetroot -solid "#333333"
+exec i3' > ~/.xinitrc
+
+
+
+# Terminal Emulator
+pacman -S rxvt-unicode --noconfirm --needed
+wget "https://raw.githubusercontent.com/arashpath/my-archi3-usb/master/dotfiles/.Xdefaults"
 
 ##
 # Use a compositor manager such as `compton` to enable effects like window fading and transparency.
@@ -28,16 +36,19 @@ echo "exec --no-startup-id feh --bg-fill ~/.config/wall.jpg" >> ~/.config/i3/con
 pacman -S rxvt-unicode rxvt-unicode-terminfo
 
 # Intalling Aura
-pacman -S base-devel wget git # ~50 MB
+pacman -S base-devel  git # ~50 MB
+
+# Littele tooles
+sudo pacman -S elinks neofetch wget htop --noconfirm --needed
 
 # Python
-pacman -S python
+sudo pacman -S python --noconfirm --needed 
 
-pacman -S htop iotop
+# tools 
+sudo pacman -S python-pywal iotop ranger --noconfirm --needed
 
-pacman -S elinks ranger
 
-pacman -S ttf-dejavu ttf-inconsolata
+
 
 # Gui tools 
 pacman -S firefox gimp vlc code
